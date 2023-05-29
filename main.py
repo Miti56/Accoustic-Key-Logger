cf8e68a787f8a21139c020ee55c6dc065d0f9488
Miti56 <61667843+Miti56@users.noreply.github.com>
2023-05-29 12:10:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class MainWindow(QMainWindow):
         # Create keys for the keyboard
         self.keys = []
         key_labels = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
-        positions = [(i, j) for i in range(4) for j in range(7)]
+        positions = [(i, j) for i in range(4) for j in range(9)]
         for label, position in zip(key_labels, positions):
             key_button = QPushButton(label, self)
             key_button.setFixedWidth(40)
