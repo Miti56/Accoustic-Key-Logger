59028e2c6c815cd57e67e43313642db502e9369d
Miti56 <61667843+Miti56@users.noreply.github.com>
2023-06-06 11:23:38 +0100

    Improved for longer record sessions

--- keypresschop.py
+++ keypresschop.py
@@ -1,3 +1,5 @@
+import random
+
 import sounddevice as sd
 import numpy as np
 import soundfile as sf
@@ -19,10 +21,10 @@ device_index = int(input("Enter the number of the microphone you want to use: ")
 
 # Set the recording parameters
 fs = 48000  # Sample rate
-seconds = 10  # Duration of recording
+seconds = 60  # Duration of recording
 channels = 1 # Number of channels used
 
-print("Press keys during the recording. Press 'esc' to finish recording.")
+print("Press keys during the recor3ding. Press 'esc' to finish recording.")
 
 keypresses = []
 start_time = time.time()
@@ -42,13 +44,13 @@ listener.start()
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=channels, device=input_devices[device_index])
 sd.wait()  # Wait until recording is finished
-sf.write('recording.wav', myrecording, fs)  # Save as WAV file
+sf.write('Long1.wav', myrecording, fs)  # Save as WAV file
 
 listener.stop()
 
 print("Creating audio clips...")
 
-song = AudioSegment.from_wav("recording.wav")
+song = AudioSegment.from_wav("Long1.wav")
 
 if not os.path.exists("clips"):
     os.makedirs("clips")
@@ -57,6 +59,7 @@ for i, (key, press_time) in enumerate(keypresses):
     start_time = int((press_time - 0.5) * 1000)  # convert to ms
     end_time = int((press_time + 0.5) * 1000)  # convert to ms
     clip = song[start_time:end_time]
-    clip.export(f"clips/{key}_{i}.wav", format="wav")
+    random_number = random.randint(1, 10000000)
+    clip.export(f"clips/{key}_{i+random_number}.wav", format="wav")
 
 print("Audio clips created!")
