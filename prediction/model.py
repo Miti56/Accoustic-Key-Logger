c7e9c0d4b6ff88aae801f35d3ddc1fec31e738af
Miti56 <61667843+Miti56@users.noreply.github.com>
2023-06-11 14:08:37 +0100

    Improvement handle probabilities

--- prediction/model.py
+++ prediction/model.py
@@ -76,57 +76,21 @@ def predict_key_press(filename, model, le):
     # Convert the predicted index to its corresponding label
     predicted_label = le.inverse_transform([predicted_index])
 
+    # Print out all the classes and their probabilities
+    sorted_indices = np.argsort(prediction[0])[::-1]  # get the indices that would sort the array, in descending order
+    print("All classes and their probabilities:")
+    for idx in sorted_indices:
+        print(f"{le.inverse_transform([idx])[0]}: {prediction[0][idx]}")
+
+
     return predicted_label[0]
+
 
 # Directory containing the audio files
 # directory2 = '/Users/miti/Documents/GitHub/Accoustic-Key-Logger/clipsCut'
-filename = '/Users/miti/Documents/GitHub/Accoustic-Key-Logger/clipsCut/e_4788655.wav'
+filename = '/Users/miti/Documents/GitHub/Accoustic-Key-Logger/record/clipsCut/o_1338622.wav'
 # for filename in os.listdir(directory2):
 predicted_key = predict_key_press(filename, model, le)
 print(f"The predicted key press for {filename} is {predicted_key}.")
 
 
-
-
-# import numpy as np
-# import sounddevice as sd
-# import librosa
-# from sklearn.preprocessing import StandardScaler
-#
-# # Chunk size in seconds
-# chunk_size = 1.0
-#
-#
-# def predict_chunk(chunk, model, le):
-#     # Convert the audio chunk into MFCCs
-#     mfccs = librosa.feature.mfcc(y=chunk, sr=sample_rate, n_mfcc=40)
-#     mfccs_processed = np.mean(mfccs.T, axis=0)
-#
-#     # # Normalize the MFCCs (if your model was trained with normalized data)
-#     # mfccs_processed = scaler.transform([mfccs_processed])
-#
-#     # Use the model to predict the label for the new audio file
-#     prediction = model.predict(mfccs_processed)
-#
-#     # Get the index of the highest predicted class
-#     predicted_index = np.argmax(prediction[0])
-#
-#     # Convert the predicted index to its corresponding label
-#     predicted_label = le.inverse_transform([predicted_index])
-#
-#     return predicted_label[0]
-#
-#
-# def callback(indata, frames, time, status):
-#     # This is called (from a separate thread) for each audio block.
-#     chunk = indata[:, 0]
-#     predicted_key = predict_chunk(chunk, model, le)
-#     print(f"Predicted key: {predicted_key}")
-#
-#
-# # Create a stream and start the microphone
-# stream = sd.InputStream(callback=callback, channels=1, samplerate=48000)
-# with stream:
-#     while True:
-#         # This is just to keep the script running
-#         sd.sleep(int(chunk_size * 1000))
