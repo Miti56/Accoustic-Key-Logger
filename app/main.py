ed0a73f415a8ea08d53b1ee61a86d5ab1fd635e2
Miti56 <61667843+Miti56@users.noreply.github.com>
2023-07-15 13:29:55 +0100

    improvements main

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
 import subprocess
-import random
 import sys
-
-import librosa
-import sounddevice as sd
-import soundfile as sf
-from pynput import keyboard
-from pydub import AudioSegment
 import time
-import scipy.io.wavfile
-from scipy.signal import find_peaks
-import os
-import numpy as np
-import scipy.io.wavfile
-from scipy.signal import correlate
+
 
 def display_prompt(prompt):
     print(prompt, end=' ')
@@ -30,6 +18,28 @@ def run_data():
         subprocess.run(['python3', 'DataVisualisation/dataVisualisation.py'], check=True)
     except subprocess.CalledProcessError:
         print("Error running the original program.")
+
+def run_modelCC():
+    try:
+        subprocess.run(['python3', 'model/modelCC.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+def run_modelML():
+    try:
+        subprocess.run(['python3', 'model/modelML.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+def display_additional_information():
+    print("Additional Information:")
+    print("- Cross-Correlation Model: This model uses cross-correlation to compare the input audio signals with known "
+          "patterns.")
+    print("It calculates the similarity between the input signals and the patterns, helping to identify specific "
+          "patterns in the audio.")
+    print("- Neural Network Model: This model utilizes a neural network architecture to analyze the audio signals.")
+    print("It learns patterns and features from the input data through training and can classify audio based on the "
+          "learned patterns.")
 
 def main():
     print("Welcome to the UI!")
@@ -55,8 +65,33 @@ def main():
     if q3:
         # Run the original program
         run_data()
+        print("Data analysis performed.")
+        q4 = display_prompt("Are the results satisfactory? ").upper() == 'Y'
+        if q4:
+            pass
+        else:
+            print("Program terminated. Try again in a quieter environment")
+            sys.exit()
+
     else:
-        print("Program terminated.")
+        print("Redirecting to the model creation...")
+        time.sleep(3)
+
+    q5 = display_prompt("Two models are currently available: Cross-Correlation or Neural Network (Press H for "
+                        "additional information").upper() == 'Y'
+
+    if q5 == 'C':
+        # Perform Cross-Correlation
+        run_modelCC()
+    elif q5 == 'N':
+        # Perform Neural Network
+        run_modelML()
+    elif q5 == 'H':
+        # Display additional information
+        display_additional_information()
+    else:
+        # Invalid input
+        print("Invalid input. Please select a valid option.")
 
 
 if __name__ == "__main__":
