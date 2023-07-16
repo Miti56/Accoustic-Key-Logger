fcf804a47793b60bad348a9c2763080e3d6d0e25
Miti56 <61667843+Miti56@users.noreply.github.com>
2023-07-16 11:56:55 +0100

    Improvements in readability and functionality

--- app/main.py
+++ app/main.py
@@ -3,9 +3,20 @@ import sys
 import time
 
 
+# ANSI escape sequences for text color
+class Color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
+
 def display_prompt(prompt):
     print(prompt, end=' ')
     return input()
+
 
 def run_record():
     try:
@@ -13,11 +24,13 @@ def run_record():
     except subprocess.CalledProcessError:
         print("Error running the original program.")
 
+
 def run_data():
     try:
         subprocess.run(['python3', 'DataVisualisation/dataVisualisation.py'], check=True)
     except subprocess.CalledProcessError:
         print("Error running the original program.")
+
 
 def run_modelCC():
     try:
@@ -25,28 +38,85 @@ def run_modelCC():
     except subprocess.CalledProcessError:
         print("Error running the original program.")
 
+
 def run_modelML():
     try:
         subprocess.run(['python3', 'model/modelML.py'], check=True)
     except subprocess.CalledProcessError:
         print("Error running the original program.")
 
+
+def run_modelVisualisation():
+    try:
+        subprocess.run(['python3', 'DataVisualisation/modelVisualisation.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+
+def run_testLiveModel():
+    try:
+        subprocess.run(['python3', 'test/liveModel.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+
+def run_testModelCC():
+    try:
+        subprocess.run(['python3', 'test/modelCCTest.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+
+def run_testModelML():
+    try:
+        subprocess.run(['python3', 'test/modelMLTest.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+
+def run_testLong():
+    try:
+        subprocess.run(['python3', 'test/testLong.py'], check=True)
+    except subprocess.CalledProcessError:
+        print("Error running the original program.")
+
+
 def display_additional_information():
+    print(f"{Color.HEADER}======================")
     print("Additional Information:")
+    print("----------------------")
     print("- Cross-Correlation Model: This model uses cross-correlation to compare the input audio signals with known "
           "patterns.")
-    print("It calculates the similarity between the input signals and the patterns, helping to identify specific "
+    print("  It calculates the similarity between the input signals and the patterns, helping to identify specific "
           "patterns in the audio.")
     print("- Neural Network Model: This model utilizes a neural network architecture to analyze the audio signals.")
-    print("It learns patterns and features from the input data through training and can classify audio based on the "
+    print("  It learns patterns and features from the input data through training and can classify audio based on the "
           "learned patterns.")
+    print("======================{Color.END}")
+
+
+def display_additional_information2():
+    print(f"{Color.HEADER}======================")
+    print("Additional Information:")
+    print("----------------------")
+    print("- Cross-Correlation Model: This model uses cross-correlation to compare the input audio signals with known "
+          "patterns.")
+    print("  It calculates the similarity between the input signals and the patterns, helping to identify specific "
+          "patterns in the audio.")
+    print("- Neural Network Model: This model utilizes a neural network architecture to analyze the audio signals.")
+    print("  It learns patterns and features from the input data through training and can classify audio based on the "
+          "learned patterns.")
+    print("======================{Color.END}")
+
 
 def main():
+    print(f"{Color.BLUE}====================")
     print("Welcome to the UI!")
 
     q1 = display_prompt("Are you in a quiet space to start the recording? ").upper() == 'Y'
 
     if q1:
+        print(f"{Color.GREEN}====================")
         # Run the original program
         run_data()
     else:
@@ -56,13 +126,14 @@ def main():
         else:
             print("Program terminated.")
             sys.exit()
+        print(f"{Color.YELLOW}====================")
         print("Redirecting...")
         time.sleep(3)
-
 
     q3 = display_prompt("Visualise Data? ").upper() == 'Y'
 
     if q3:
+        print(f"{Color.GREEN}====================")
         # Run the original program
         run_data()
         print("Data analysis performed.")
@@ -74,26 +145,85 @@ def main():
             sys.exit()
 
     else:
+        print(f"{Color.YELLOW}====================")
         print("Redirecting to the model creation...")
         time.sleep(3)
 
-    q5 = display_prompt("Two models are currently available: Cross-Correlation or Neural Network (Press H for "
-                        "additional information").upper() == 'Y'
+    while True:
+        q5 = display_prompt("Two models are currently available: Cross-Correlation (CC) or Neural Network (NN "
+                            "(Press H for additional information)").upper()
 
-    if q5 == 'C':
-        # Perform Cross-Correlation
-        run_modelCC()
-    elif q5 == 'N':
-        # Perform Neural Network
-        run_modelML()
-    elif q5 == 'H':
-        # Display additional information
-        display_additional_information()
+        if q5 == 'CC':
+            print(f"{Color.GREEN}====================")
+            # Perform Cross-Correlation
+            run_modelCC()
+            break
+        elif q5 == 'NN':
+            print(f"{Color.GREEN}====================")
+            # Perform Neural Network
+            run_modelML()
+            break
+        elif q5 == 'H':
+            print(f"{Color.HEADER}====================")
+            # Display additional information
+            display_additional_information()
+        else:
+            # Invalid input
+            print("Invalid input. Please select a valid option.")
+
+    print(f"{Color.GREEN}====================")
+    print("Model has been trained!")
+    print("Redirecting to the model visualisation...")
+    time.sleep(3)
+
+    q6 = display_prompt("Visualise Model? ").upper() == 'Y'
+
+    if q6:
+        print(f"{Color.GREEN}====================")
+        # Run the original program
+        run_modelVisualisation()
+        print("Model analysis performed.")
+        q4 = display_prompt("Are the results satisfactory? ").upper() == 'Y'
+        if q4:
+            pass
+        else:
+            print("Program terminated. Try again in a quieter environment")
+            sys.exit()
+
     else:
-        # Invalid input
-        print("Invalid input. Please select a valid option.")
+        print(f"{Color.YELLOW}====================")
+        print("Redirecting to the testing suite...")
+        time.sleep(3)
+
+    while True:
+        print(f"{Color.BLUE}====================")
+        print("Which test do you wish to perform?")
+        print("Press H for a description of the different tests.")
+        print("Press Q to end the tests")
+        q7 = display_prompt("Test using: Live Model (LM), ML Model (ML), CC Model (CC) or Wav File Test (WT)?").upper()
+
+        if q7 == 'LM':
+            print(f"{Color.GREEN}====================")
+            run_testLiveModel()
+        elif q7 == 'ML':
+            print(f"{Color.GREEN}====================")
+            run_testModelML()
+        elif q7 == 'CC':
+            print(f"{Color.GREEN}====================")
+            run_testModelCC()
+        elif q7 == 'WT':
+            print(f"{Color.GREEN}====================")
+            run_testLong()
+        elif q7 == 'H':
+            print(f"{Color.HEADER}====================")
+            # Display additional information
+            display_additional_information2()
+        elif q7 == 'Q':
+            sys.exit()
+        else:
+            # Invalid input
+            print("Invalid input. Please select a valid option.")
 
 
 if __name__ == "__main__":
     main()
-
